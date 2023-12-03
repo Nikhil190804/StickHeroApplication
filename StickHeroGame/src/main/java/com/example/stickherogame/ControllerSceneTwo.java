6fadc9a07e19f0cfa6969b2e8a424c57fadd0fde
Niketiiitd <niket22320@iiitd.ac.in>
2023-12-04 03:45:07 +0530

    Update ControllerSceneTwo.java
    Player render fixed

--- StickHeroGame/src/main/java/com/example/stickherogame/ControllerSceneTwo.java
+++ StickHeroGame/src/main/java/com/example/stickherogame/ControllerSceneTwo.java
@@ -105,18 +105,12 @@ public class ControllerSceneTwo implements Initializable {
     }
 
     public void renderPlayerObject(Player player){
-        Platform platformPlayerisON = Game.platforms.peek();
-        double mid_point = platformPlayerisON.getStartingX() + (platformPlayerisON.getWidth()/2);
-        player.setLayoutX(mid_point);
-        player.setLayoutY(250);
-        parent_anchorpane.getChildren().add(player);
-    }
-    public void renderPlayerObjectFirstTime(Player player){
-        //player.setLayoutX(player.getX_coordinate());
-        //player.setLayoutY(208);
-        parent_anchorpane.getChildren().add(player);
 
+        player.setLayoutX(player.getX_coordinate()- 25 );
+        player.setLayoutY(208);
+        parent_anchorpane.getChildren().add(player);
     }
+
 
     private void print(MouseEvent event) {
         System.out.println("start");
