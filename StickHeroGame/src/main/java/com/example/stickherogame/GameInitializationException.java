eebfdbcc4383d4f7aea5e485f11182082c2d4141
Niketiiitd <niket22320@iiitd.ac.in>
2023-12-09 23:51:32 +0530

    Delete GameInitializationException.java

--- StickHeroGame/src/main/java/com/example/stickherogame/GameInitializationException.java
+++
@@ -1,9 +0,0 @@
-package com.example.stickherogame;
-
-public class GameInitializationException extends Exception{
-    public GameInitializationException(String message) {
-        super(message);
-    }
-
-
-}
