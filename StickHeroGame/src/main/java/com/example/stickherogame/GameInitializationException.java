b3211e2c841f304df82b46260504f0e1f31829cb
Niketiiitd <niket22320@iiitd.ac.in>
2023-12-09 20:11:11 +0530

    Create GameInitializationException.java

---
+++ StickHeroGame/src/main/java/com/example/stickherogame/GameInitializationException.java
@@ -0,0 +1,9 @@
+package com.example.stickherogame;
+
+public class GameInitializationException extends Exception{
+    public GameInitializationException(String message) {
+        super(message);
+    }
+
+
+}
